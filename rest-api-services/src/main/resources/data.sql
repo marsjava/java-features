05b76b1fe3082a5c117bf2c0810ff0886ef685ee
Marikannan <marikannan.cvp@gmail.com>
2021-07-03 17:49:07 +0530

    rest-api create post entity and apply crud operation

--- rest-api-services/src/main/resources/data.sql
+++ rest-api-services/src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into user values(1, sysdate(), 'Antony');
-insert into user values(2, sysdate(), 'Ivan');
-insert into user values(3, sysdate(), 'Ganesh');
+insert into user values(101, sysdate(), 'Antony');
+insert into user values(102, sysdate(), 'Ivan');
+insert into user values(103, sysdate(), 'Ganesh');
+insert into post values(1001, 'My First Post', 101);
+insert into post values(1002, 'My Second Post', 101);
